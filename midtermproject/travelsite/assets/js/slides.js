72eb7b30997bf28bab55058daf38c9d42fa066c9
NamDell <namkhanh.nguyen@edu.sait.ca>
2020-06-26 14:14:22 -0600

    Finish Part 2 Midterm

--- midtermproject/travelsite/assets/js/slides.js
+++ midtermproject/travelsite/assets/js/slides.js
@@ -5,9 +5,9 @@ window.onload = showSlides;
 
 //global variables
 
-var myPix = new Array("assets/images/exner.jpg", "assets/images/countach.jpg", "assets/images/shelby_daytona.jpg", 
+var myPix = new Array("assets/images/bike.jpg", "assets/images/coding.jpg", "assets/images/piano.jpg", 
 "assets/images/coffeBeautiful.jpg", "assets/images/coffecup.jpg", "assets/images/coffeEat.jpg", "assets/images/coffeFlower.jpg", "assets/images/coffeSeed.jpg");
-var figure = new Array("Exner", "Countach", "Shelby Daytona", "Beautiful Coffe", "Cup Of Coffee", "Eating Coffee", "Flower Coffee", "Seed Coffee");
+var figure = new Array("Bike", "Coding", "Learning Piano", "Beautiful Coffe", "Cup Of Coffee", "Eating Coffee", "Flower Coffee", "Seed Coffee");
 var thisPic = 0;
 
 function showSlides() {
@@ -18,8 +18,8 @@ function showSlides() {
 		setTimeout(showSlides, 2000);
 	} else {
 		thisPic = 0;
-		document.getElementById("text").innerHTML = figure[thisPic];
 		document.getElementById("myPicture").src = myPix[thisPic];
+		document.getElementById("text").innerHTML = figure[thisPic];
 		thisPic++;
 		setTimeout(showSlides, 2000);
 	}
